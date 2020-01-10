51584e09ea59adeb8714806d751c2c7ac36c023c
DevonMartens <dmmartens91@gmail.com>
2020-01-09 20:21:46 -0500

    TEST

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const app = require('express').Router();
 
-var db = require("../models");
 
 var cheerio = require('cheerio');
 const axios = require('axios')
+var db = require("../models");
 // Routes
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
